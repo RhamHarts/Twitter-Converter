9e7bebdf675449b2615501e62cb38c3fbf67d2ab
Rhamharts <mhmirham22@gmail.com>
2024-07-31 21:00:12 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        twitterBlue: '#1DA1F2',
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+};
+
+
